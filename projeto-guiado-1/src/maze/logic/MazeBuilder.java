695f7487e6875551d48a4693c3bb5e4cbe5273b4
João Silva <j.pedro004@gmail.com>
2016-03-06 22:49:53 +0000

    added maze builder files

---
+++ projeto-guiado-1/src/maze/logic/MazeBuilder.java
@@ -0,0 +1,11 @@
+package maze.logic;
+
+public class MazeBuilder implements IMazeBuilder {
+
+	@Override
+	public char[][] buildMaze(int size) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
